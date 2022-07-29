8089ede26500ec34536c95b92b0817cfc2f3fda0
montoyafranco <francomontoya9810@gmail.com>
2022-07-29 03:21:03 -0300

    fetch working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 "console"
+export async function getAllNotes() {
+    const response:Response = await fetch('http://localhost:8081/api/')
+  
+    const data:[] = await response.json()
+    console.log(data)
+  
+    return data
+  } 
+
+  getAllNotes()
+  
